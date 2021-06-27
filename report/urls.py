28ab46f4887112ab87ed03b6b65b3af3b1d72fbb
Mohammed Al-Turki <81957308+Mohammed-Alturki@users.noreply.github.com>
2021-06-27 00:03:23 +0000

    Views url path html for 3 pages

--- report/urls.py
+++ report/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('all/reports', views.all_reports),
+    path('all/receiver', views.all_receiver),
+    path('all/roads', views.all_roads)
+
 ]
--- report/views.py
+++ report/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Report
-
+from .models import Report   
+from .models import Receiver
+from .models import Roads
 # Create your views here.
 
 
@@ -11,3 +12,15 @@ def all_reports(request):
   #data['location'] = reports.objects.get_location_display()
   #data['location'] = reports.objects.get_location_display()
   return render(request, "all_reports.html", context=data)
+
+def all_receiver(request):
+  data={}
+  receiver = Receiver.objects.all
+  data['receiver'] = receiver
+  return render(request, "all_receiver.html", context=data)
+
+def all_roads(request):
+  data={}
+  roads = Roads.objects.all
+  data['roads'] = roads
+  return render(request, "all_roads.html", context=data)
